f0576fc071bc05900678c925f24fb605b583188a
chunchill <jasper.qiu@guanshantech.com>
2017-07-03 16:26:07 +0800

    remove the unused functions

--- src/api-gateway/api.py
+++ src/api-gateway/api.py
@@ -5,11 +5,6 @@ app = Flask(__name__)
 CONFIG = {'AMQP_URI': "amqp://xthidnmx:naMqLqyPbJlrtYBkm-ZTYcdiIpZcwJsC@fish.rmq.cloudamqp.com/xthidnmx"}
 
 
-@app.route('/index',methods=['GET'])
-def index():
-    results = [{'name':'jasper','age':'10'},{'name':'kim','age':'20'}]
-    return render_template('result.html',people=results), 200
-
 @app.route('/',methods=['GET'])
 def run_test():
     file = open('test_cases/testcase_001.yaml','r')
@@ -27,13 +22,6 @@ def run_test():
             'description': step['description'],
             'result': result
         })
-    # test_results = output(results)
     return render_template('result.html', results=results),200
-
-def output(results):
-    test_result = ''
-    for item in results:
-        test_result += item['description'] + ', and ' + item['result'] + '<br/>'
-    return test_result
 
 app.run(debug=True,port=8080)
